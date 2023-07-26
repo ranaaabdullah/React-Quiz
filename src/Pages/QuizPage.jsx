449177922c850461f44a82ff34c553eecbce34ea
Muhammad Abdullah <rana.abdullah3655@gmail.com>
2023-07-26 17:11:43 +0500

    Fixed

--- src/Pages/QuizPage.jsx
+++ src/Pages/QuizPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import questions from "../quiz.json";
 
-const QuizPage = ({handleScore ,handleSetPage}) => {
+const QuizPage = ({ handleScore, handleSetPage }) => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [isSelectedOption, setIsSelectedOption] = useState(false);
   const [clickedOption, setClickedOption] = useState(null);
@@ -12,49 +12,45 @@ const QuizPage = ({handleScore ,handleSetPage}) => {
   const { question, options, answer } = questions[currentQuestion];
 
   const handleClickNext = () => {
-    if(currentQuestion===9)
-    {handleSetPage("Result")}
-    setCurrentQuestion(currentQuestion + 1);
-    setIsSelectedOption(false)
-    setClickedOption(null)
-    setClickedIndex(null)
-    if(correctAnswer){
-      handleScore()
+    if (currentQuestion === 9) {
+      handleSetPage("Result");
     }
-    
-
+    setCurrentQuestion(currentQuestion + 1);
+    setIsSelectedOption(false);
+    setClickedOption(null);
+    setClickedIndex(null);
+    if (correctAnswer) {
+      handleScore();
+    }
   };
-  
 
   const handleOptionClick = (option, index) => {
-    console.log(option);
     setClickedOption(option);
     setClickedIndex(index);
     setIsSelectedOption(true);
   };
 
   const correctAnswer = clickedOption === answer;
-  
-  
-  
+
   const renderedOptions = options.map((option, index) => {
-      const correctOption = option === answer;
+    const correctOption = option === answer;
     return (
       <button
         key={index}
         style={{
-
           backgroundColor: isSelectedOption
-            ?clickedIndex===index?
-             correctAnswer
-              ? "green"
-              : "red":
-              ""
+            ? clickedIndex === index
+              ? correctAnswer
+                ? "green"
+                : "red"
+              : ""
             : "",
         }}
         disabled={isSelectedOption}
         onClick={() => handleOptionClick(option, index)}
-        className={`${isSelectedOption&& correctOption && 'bg-green-700'} bg-zinc-800 text-white text-start mb-5 p-2 rounded-md`}
+        className={` bg-zinc-800   text-white text-start mb-5 p-2 rounded-md ${
+          isSelectedOption && correctOption && " correct-style"
+        }`}
       >
         {option}
       </button>
@@ -78,7 +74,6 @@ const QuizPage = ({handleScore ,handleSetPage}) => {
               className="bg-blue-500 text-white font-bold w-full mt-4 h-9 rounded-lg "
               onClick={handleClickNext}
               disabled={!isSelectedOption}
-
             >
               {" "}
               Next Question{" "}
